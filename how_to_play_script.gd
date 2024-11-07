ef2281361b7d5c71836abcb783f2e74a2e7c9088
CayG22 <136837724+CayG22@users.noreply.github.com>
2024-11-07 11:55:20 -0600

    Finished How to play page, connected it to main game

--- how_to_play_script.gd
+++ how_to_play_script.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _ready():
+	$phaseOneVid.play()
+	$targetsVid.play()
+	$arrowsVid.play()
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://title_screen.tscn")
--- main.gd
+++ main.gd
@@ -32,6 +32,9 @@ func _on_options_button_pressed() -> void:
 	#$ClickSound.play()
 	get_tree().change_scene_to_file("res://optionsMenu.tscn")
 
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://howToPlay.tscn")
+
 func _on_play_button_mouse_entered() -> void:
 	$HoverSound.play()
 
